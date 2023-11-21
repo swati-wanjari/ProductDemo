3ba5b9068e09dc97e68e4c1182c2ab041fd920fa
swati-wanjari <swati.wanjari@smarbl.com>
2023-11-21 09:42:37 +0530

    Added Docker Compose

--- Run_Db_img.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker stop my-postgres-container
-docker rm my-postgres-container
-docker run --name=my-postgres-container -d -p 5000:5432 mypostgre:latest
--- product_image_build.sh
+++
@@ -1,7 +0,0 @@
-cd DbDocker
-docker build --tag=mypostgre:latest .
-
-# shellcheck disable=SC2103
-cd ..
-docker build --tag=productservice:latest .
-
